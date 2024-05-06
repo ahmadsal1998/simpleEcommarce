2600eed08174ac26669f85e9deea2c32cce45842
ahmadsal1998 <salamea015@gmail.com>
2024-05-06 17:19:55 +0300

    finish contact page and send Enquiries

--- frontend/src/features/products/productService.js
+++ frontend/src/features/products/productService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { base_url, config } from "../../utils/axiosConfig";
 
-const getProducts = async () => {
+const getProducts = async (userData) => {
   const response = await axios.get(`${base_url}product`,);
   if (response.data) {
     return response.data;
